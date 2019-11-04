44e650b34686237a5781a37e235534ccc6baf9db
Angus Morrison <contact@angus-morrison.com>
2019-11-04 11:40:58 +0000

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,9 +2,34 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
+
+  const link = {
+    width: '100px',
+    padding: '12px',
+    margin: '0 6px 6px',
+    background: 'blue',
+    textDecoration: 'none',
+    color: 'white',
+  }
+
+  const activeLink = {
+    background: "darkblue"
+  }
+
   return (
-    <div>
-      {/*{code here}*/}
+    <div className="navbar">
+      <NavLink exact to="/" style={link} activeStyle={activeLink}>
+        Home
+      </NavLink>
+      <NavLink exact to="/movies" style={link} activeStyle={activeLink} >
+        Movies
+      </NavLink>
+      <NavLink exact to="/directors" style={link} activeStyle={activeLink} >
+        Directors
+      </NavLink>
+      <NavLink exact to="/actors" style={link} activeStyle={activeLink} >
+        Actors
+      </NavLink>
     </div>
   );
 };
